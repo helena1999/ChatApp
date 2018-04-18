64dc5c58dbc95b0a1a687865ff1810423b911a7d
Opilane TTHK <opilane@tthk.ee>
2018-04-18 14:15:45 +0300

    1

---
+++ app/src/main/java/com/example/opilane/chatapp/ChatMessage.java
@@ -0,0 +1,47 @@
+package com.example.opilane.chatapp;
+
+import java.util.Date;
+
+/**
+ * Created by opilane on 18.04.2018.
+ */
+
+public class ChatMessage {
+
+    private String messageText;  //sõnumi tekstiks
+    private String messageUser;  //sõnumi saajaks
+    private long messageTime;    //sõnumi ajaks
+
+    public ChatMessage(String messageText, String messageUser, long messageTime) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+        messageTime = new Date().getTime();
+    }
+
+    public ChatMessage() {
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
